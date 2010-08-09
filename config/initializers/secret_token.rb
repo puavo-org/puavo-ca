881623721dcc3c2775e2a9d3dedfc947776c7001
Jouni Korhonen <jouni.korhonen@opinsys.fi>
2010-08-09 15:00:14 +0300

    Initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PuavoCa::Application.config.secret_token = 'af65f3d518eabf9e3b7e48b2bf3abd8c5e1c026711661da9edb7cfcaea1b44b19935c0dc3213880490654e8733b213f787f6c4a8a35af9f1978bfa2648ba835e'

36942b47b8caa41d63bb09ffb9b986b396255951
Jouni Korhonen <jouni.korhonen@gmail.com>
2010-08-12 13:04:15 +0300

    Added LdapUser class

---
+++ app/models/ldap_user.rb
@@ -0,0 +1,23 @@
+class LdapUser
+  def self.authenticate(dn, password)
+    logger.debug "Ldap Authentication:"
+    logger.debug "dn: #{dn}"
+
+    begin
+      ldap = LDAP::Conn.new(host=PUAVO_CONFIG['ldap_server'])
+      ldap.set_option(LDAP::LDAP_OPT_PROTOCOL_VERSION, 3)
+      return true if ldap.bind(dn, password)
+
+      # FIXME:  Allow authetication only if user is School Admin in the some School or organisation owner.
+    rescue Exception => e
+      logger.info "Authentication failed, Exception: #{e}"
+      return false
+    end
+  end
+
+  private
+
+  def self.logger
+    Rails.logger
+  end
+end

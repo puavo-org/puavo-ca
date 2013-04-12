97b0d3d05311f18e065d196ea48e0797f0a26476
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-04-12 12:57:48 +0000

    rails: fix puavo.yml path

--- rails/config/initializers/puavo_configuration.rb
+++ rails/config/initializers/puavo_configuration.rb
@@ -2,6 +2,6 @@
 begin
     PUAVO_CONFIG = YAML.load_file("#{Rails.root}/config/puavo.yml")
 rescue Errno::ENOENT
-    PUAVO_CONFIG = YAML.load_file("/etc/puavo-ca/puavo.yml")
-    STDERR.puts "Using puavo config from /etc/puavo-ca/puavo.yml"
+    PUAVO_CONFIG = YAML.load_file("/etc/puavo-ca-rails/puavo.yml")
+    STDERR.puts "Using puavo config from /etc/puavo-ca-rails/puavo.yml"
 end

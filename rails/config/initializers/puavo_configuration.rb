9cca517510bb17053bfe920412bdd27908021187
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-04-15 16:04:31 +0300

    rails: keep configs in rails dir (puavo.yml)

--- rails/config/initializers/puavo_configuration.rb
+++ rails/config/initializers/puavo_configuration.rb
@@ -1,7 +1,2 @@
 
-begin
-    PUAVO_CONFIG = YAML.load_file("#{Rails.root}/config/puavo.yml")
-rescue Errno::ENOENT
-    PUAVO_CONFIG = YAML.load_file("/etc/puavo-ca-rails/puavo.yml")
-    STDERR.puts "Using puavo config from /etc/puavo-ca-rails/puavo.yml"
-end
+PUAVO_CONFIG = YAML.load_file("#{Rails.root}/config/puavo.yml")

236783b92521521d49c1c96aee913387c8cfb1e4
Esa-Matti Suuronen <esa-matti@suuronen.org>
2013-04-18 07:00:03 +0000

    rails: Read ldap server config from /etc/puavo if missing

--- rails/config/initializers/puavo_configuration.rb
+++ rails/config/initializers/puavo_configuration.rb
@@ -1,2 +1,7 @@
 
 PUAVO_CONFIG = YAML.load_file("#{Rails.root}/config/puavo.yml")
+
+if PUAVO_CONFIG["ldap_server"].to_s.empty?
+  require "puavo/etc"
+  PUAVO_CONFIG["ldap_server"] = PUAVO_ETC.ldap_master
+end

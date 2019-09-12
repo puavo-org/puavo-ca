e1dad43e5c8f64dca0fb6a7478ac09da5721aa2b
Juha Erkkilä <Juha.Erkkila@opinsys.fi>
2019-09-12 09:54:00 +0300

    Make rootca and orgcabundle interfaces versioned so that if certificate chain version is given as a parameter, lookup that version, otherwise use the default version (configurable in the puavo-ca configuration file).

--- rails/app/controllers/certificates_controller.rb
+++ rails/app/controllers/certificates_controller.rb
@@ -1,42 +1,68 @@
 require 'puavo/etc'
 
 class CertificatesController < ApplicationController
-  before_action :require_http_auth_user, :except => [:rootca, :orgcabundle]
+  before_action :require_http_auth_user, :except => [ :orgcabundle, :rootca ]
 
   # GET /certificates/rootca.text
   def rootca
+    version = params[:version]
+    return render_badparam('bad format in version parameter') \
+      unless version.nil? || version.match(/\A\d+\z/)
+
+    cert_subpath = "rootca/ca.#{ PUAVO_ETC.topdomain }.crt"
 
     respond_to do |format|
-      format.text  { send_data( File.read("#{ PUAVO_CONFIG['certdirpath'] }/rootca/ca.#{ PUAVO_ETC.topdomain }.crt"), :type => 'text/plain' ) }
+      format.text do
+        send_data(get_cert(cert_subpath, version),
+                  :type => 'text/plain')
+      end
     end
   end
 
   # GET /certificates/orgcabundle.text
   def orgcabundle
+    org = params[:org]
+    return render_badparam('bad format in organisation parameter') \
+      unless org.match(/\A\w+\z/)
+
+    version = params[:version]
+    return render_badparam('bad format in version parameter') \
+      unless version.nil? || version.match(/\A\d+\z/)
+
+    topdomain = PUAVO_ETC.topdomain
+    cert_subpath = "organisations/ca.#{ org }.#{ topdomain }-bundle.pem"
 
     respond_to do |format|
-      format.text  { send_data( File.read("#{ PUAVO_CONFIG['certdirpath'] }/organisations/ca.#{ params[:org] }.#{ PUAVO_ETC.topdomain}-bundle.pem"), :type => 'text/plain' ) }
+      format.text do
+        send_data(get_cert(cert_subpath, version),
+                  :type => 'text/plain')
+      end
     end
   end
 
+  # XXX what is this used for?  each device in an organisation can get a list
+  # XXX of all certificates for all organisations???? (if also puavo-rest
+  # XXX would allow such a call)
   # GET /certificates.json
   def index
     @certificates = Certificate.all
 
     respond_to do |format|
-      format.json  { render :json => @certificates }
+      format.json { render :json => @certificates }
     end
   end
 
+  # XXX is this used for something?
   # GET /certificates/revoked_list.json
   def revoked_list
     @certificates = Certificate.where(:revoked => true)
     
     respond_to do |format|
-      format.json  { render :json => @certificates }
+      format.json { render :json => @certificates }
     end
   end
 
+  # XXX is this used for something?
   # GET /certificates/1.json
   def show
     @certificate = Certificate.find(params[:id])
@@ -54,7 +80,7 @@ class CertificatesController < ApplicationController
       if @certificate
         format.json { render :json => { 'certificate' => @certificate } }
       else
-        format.json { render :json => "404 Not Found", :status => :not_found }
+        format.json { render :json => '404 Not Found', :status => :not_found }
       end
     end
   end
@@ -66,10 +92,15 @@ class CertificatesController < ApplicationController
     
     respond_to do |format|
       if @certificate.save
-        format.json { render :json   => { 'certificate' => @certificate },
-                             :status => :created }
+        format.json do
+          render :json   => { 'certificate' => @certificate },
+                 :status => :created
+        end
       else
-        format.json { render :json => @certificate.errors, :status => :unprocessable_entity }
+        format.json do
+          render :json   => @certificate.errors,
+                 :status => :unprocessable_entity
+        end
       end
     end
   end
@@ -85,9 +116,9 @@ class CertificatesController < ApplicationController
 
     respond_to do |format|
       if @certificate
-        format.json  { head :ok }
+        format.json { head :ok }
       else
-        format.json  { render :json => "404 Not Found", :status => :not_found }
+        format.json { render :json => '404 Not Found', :status => :not_found }
       end
     end
   end
@@ -95,17 +126,35 @@ class CertificatesController < ApplicationController
   # DELETE /certificates/test_clean_up
   def test_clean_up
     if Rails.env.test?
-      Certificate.find_all_by_organisation("example").each do |certificate|
+      Certificate.find_all_by_organisation('example').each do |certificate|
         certificate.destroy
       end
       render :json => true
     else
-      render :json => {:error => "internal-server-error"}.to_json, :status => 500
+      render :json   => { :error => 'internal-server-error'}.to_json,
+             :status => 500
     end
   end
 
   private
+
   def certificate_params
     params.require(:certificate).permit(:fqdn, :host_certificate_request)
+  end
+
+  def get_cert(cert_subpath, version)
+    if version.nil? then
+      version = PUAVO_CONFIG['default_certchain_version']
+      raise 'no certchain version' if version.nil?
+    end
+
+    certdir  = "#{ PUAVO_CONFIG['certdirpath'] }/#{ version }"
+    certpath = "#{ certdir }/#{ cert_subpath }"
+    File.read(certpath)
+  end
+
+  def render_badparam(errmsg)
+    render :json   => { :error => errmsg }.to_json,
+           :status => 400
   end
 end
